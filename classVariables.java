c70508f046e4935f3520961f71906d2def0dfb8d
Manisha25Kalota <109798411+Manisha25Kalota@users.noreply.github.com>
2022-10-08 10:59:26 +0530

    Add files via upload

---
+++ classVariables.java
@@ -0,0 +1,12 @@
+public class classVariables {
+    public static void main(String [] args){
+        //variables
+        int a = 50;
+        int b= 10;
+        int sum = a+b;
+        //System.out.println(sum);
+
+        int mul = a * b;
+        System.out.println(mul);
+    }
+}
